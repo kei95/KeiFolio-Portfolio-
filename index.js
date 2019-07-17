3f8429854625c4b3720361b2f25c3a2a06db2b4f
Kei95 <kei0613shine.star@gmail.com>
2019-07-16 20:46:56 -0700

    deleted read me and moved index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './src/App';
+import * as serviceWorker from './src/serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
