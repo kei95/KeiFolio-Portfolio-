490aa3d7a54c6efc711af203ef4e6160077ed8f5
Kei95 <kei0613shine.star@gmail.com>
2019-07-17 08:02:49 -0700

    Updates

---
+++ precache-manifest.03169cd3121f16d29718846f1d14be4c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc556288df2e0f6a20fea97595f7aaa9",
+    "url": "/KeiFolio-Portfolio-/index.html"
+  },
+  {
+    "revision": "cfea0c5be25d42aea647",
+    "url": "/KeiFolio-Portfolio-/static/css/2.65f232cb.chunk.css"
+  },
+  {
+    "revision": "5ceaaabcb1bc2defde73",
+    "url": "/KeiFolio-Portfolio-/static/css/main.2effa050.chunk.css"
+  },
+  {
+    "revision": "cfea0c5be25d42aea647",
+    "url": "/KeiFolio-Portfolio-/static/js/2.e723132b.chunk.js"
+  },
+  {
+    "revision": "5ceaaabcb1bc2defde73",
+    "url": "/KeiFolio-Portfolio-/static/js/main.c01b9ac2.chunk.js"
+  },
+  {
+    "revision": "c2d298294244d34b70d3",
+    "url": "/KeiFolio-Portfolio-/static/js/runtime~main.9a2f3add.js"
+  },
+  {
+    "revision": "d91db964c5bcf7249bdfcf2c22c0cffb",
+    "url": "/KeiFolio-Portfolio-/static/media/Cards_myself.d91db964.png"
+  },
+  {
+    "revision": "e494c7e7cc7bef93460d300eb202d12c",
+    "url": "/KeiFolio-Portfolio-/static/media/Cards_pic1.e494c7e7.jpg"
+  },
+  {
+    "revision": "ea9be5e88ea7d9a7a41d4e2b778a08a1",
+    "url": "/KeiFolio-Portfolio-/static/media/Cards_pic2.ea9be5e8.jpg"
+  },
+  {
+    "revision": "5b26c219cb651f0e5aa3718ff67a2a50",
+    "url": "/KeiFolio-Portfolio-/static/media/Cards_pic3.5b26c219.jpg"
+  },
+  {
+    "revision": "94dd608c8a59859ddd9990282a7efe17",
+    "url": "/KeiFolio-Portfolio-/static/media/Logo-kei.94dd608c.jpg"
+  },
+  {
+    "revision": "97daed86922455afc2ac28f9d9da88b0",
+    "url": "/KeiFolio-Portfolio-/static/media/MyArt1.97daed86.jpg"
+  },
+  {
+    "revision": "8fe812a893bad72e0db093f35e8f42e4",
+    "url": "/KeiFolio-Portfolio-/static/media/MyArt2.8fe812a8.jpg"
+  },
+  {
+    "revision": "fc04855f7b26a1fef7fb7371ade777b6",
+    "url": "/KeiFolio-Portfolio-/static/media/MyArt3.fc04855f.jpeg"
+  },
+  {
+    "revision": "7411f708a935664347e60f78a696de18",
+    "url": "/KeiFolio-Portfolio-/static/media/Portfolio-Pic.7411f708.jpg"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/KeiFolio-Portfolio-/static/media/fa-solid-900.f14c3b2f.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c8b1c761a3def153a326a23bc62c499.js"
+  "/KeiFolio-Portfolio-/precache-manifest.03169cd3121f16d29718846f1d14be4c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KeiFolio-Portfolio-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
