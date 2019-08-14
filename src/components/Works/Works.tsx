53f0504d0a1ffb0e4292229aaffd5af5f3cdf4c0
Kei95 <kei0613shine.star@gmail.com>
2019-08-14 08:03:58 -0700

    Add project

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -9,6 +9,7 @@ import MyArt3 from '../../MyArt3.jpeg';
 import Card1 from '../../ProjectMS.jpg';
 import Card2 from '../../DrinkerPicker.jpg';
 import Card3 from '../../HireMe.jpg';
+import Card4 from '../../Base.jpg';
 
 interface Props {
     getOffsetsFromSctions: any;
@@ -22,128 +23,142 @@ class Works extends Component<Props> {
 
     componentDidMount() {
         var rect = reactDOM.findDOMNode(this)
-    this.props.getOffsetsFromSctions(rect.offsetTop)
+        this.props.getOffsetsFromSctions(rect.offsetTop)
     }
 
     render() {
         const opts = {
             playerVars: {
-            autoplay: 1
+                autoplay: 1
             }
         };
-        return(
+        return (
             <div>
 
                 <div className="Title-Ability" >
-                <span style={{textAlign: 'center', fontSize: '3rem'}}>Projects</span>
-                <p className="Discription-Ability" >Projects that I worked/work on.</p>
-                <div style={{alignContent: 'center'}} className="Border-Ability" />
-                </div> 
+                    <span style={{ textAlign: 'center', fontSize: '3rem' }}>Projects</span>
+                    <p className="Discription-Ability" >Projects that I worked/work on.</p>
+                    <div style={{ alignContent: 'center' }} className="Border-Ability" />
+                </div>
 
 
 
                 <div className="Cards-Container col-xl-9 mx-auto">
-                <CardDeck>
-                    <Card className="card">
-                        <div className="box-shadows">
-                        <Card.Img className=" CardImage " variant="top" src={Card1} />
-                        <Card.Body>
-                            <Card.Title>ProjectMS</Card.Title>
-                            <Card.Text>
-                            Desktop app that aims to increase productivity and focus. 
-                            It provides relaxing music and motivational quotes with an 
-                            adjustable timer.
+                    <CardDeck>
+                        <Card className="card">
+                            <div className="box-shadows">
+                                <Card.Img className=" CardImage " variant="top" src={Card1} />
+                                <Card.Body>
+                                    <Card.Title>ProjectMS</Card.Title>
+                                    <Card.Text>
+                                        Desktop app that aims to increase productivity and focus.
+                                        It provides relaxing music and motivational quotes with an
+                                        adjustable timer.
                             </Card.Text>
-                            <a href="https://github.com/kei95/ProjectMS">
-                            <Button variant="primary">Chack the app</Button>
-                            </a>
-                        </Card.Body>
-                        </div>
-                    </Card>
+                                    <a href="https://github.com/kei95/ProjectMS">
+                                        <Button variant="primary">Chack the app</Button>
+                                    </a>
+                                </Card.Body>
+                            </div>
+                        </Card>
 
-                    <Card>
-                    <div className="box-shadows">
-                        <Card.Img className=" CardImage " variant="top" src={Card2} />
-                        <Card.Body>
-                            <Card.Title>DrinkerPicker</Card.Title>
-                            <Card.Text>
-                            Mobile game(Number picker) designed for a small number 
-                            of people to use in a party setting for fun. made by
-                            React Native.
+                        <Card>
+                            <div className="box-shadows">
+                                <Card.Img className=" CardImage " variant="top" src={Card2} />
+                                <Card.Body>
+                                    <Card.Title>DrinkerPicker</Card.Title>
+                                    <Card.Text>
+                                        Mobile game(Number picker) designed for a small number
+                                        of people to use in a party setting for fun. made by
+                                        React Native.
                             </Card.Text>
-                            <a href="https://github.com/kei95/DrinkerPicker-ReactNaive-">
-                            <Button variant="primary">Chack the app</Button>
-                            </a>
-                        </Card.Body>
-                        </div>
-                    </Card>
+                                    <a href="https://github.com/kei95/DrinkerPicker-ReactNaive-">
+                                        <Button variant="primary">Chack the app</Button>
+                                    </a>
+                                </Card.Body>
+                            </div>
+                        </Card>
 
-                    <Card>
-                    <div className="box-shadows">
-                    <Card.Img className=" CardImage " variant="top" src={Card3} />
-                        <Card.Body>
-                            <Card.Title>HireMe</Card.Title>
-                            <Card.Text>
-                            Mobile app to keep track of your resume that you sent to company/organization. 
-                            made by Java(Android studio).
+                        <Card>
+                            <div className="box-shadows">
+                                <Card.Img className=" CardImage " variant="top" src={Card3} />
+                                <Card.Body>
+                                    <Card.Title>HireMe</Card.Title>
+                                    <Card.Text>
+                                        Mobile app to keep track of your resume that you sent to company/organization.
+                                        made by Java(Android studio).
                             </Card.Text>
-                            <a href="https://github.com/Rtihai/HireMe">
-                            <Button variant="primary">Chack the app</Button>
-                            </a>
-                        </Card.Body>
+                                    <a href="https://github.com/Rtihai/HireMe">
+                                        <Button variant="primary">Chack the app</Button>
+                                    </a>
+                                </Card.Body>
+                            </div>
+                        </Card>
+                    </CardDeck>
+
+                    <div style={{marginTop: '3rem'}} className="box-shadows">
+                            <Card.Img className=" CardImage " variant="top" src={Card4} />
+                            <Card.Body>
+                                <Card.Title>Base</Card.Title>
+                                <Card.Text>
+                                A scalable software that is offered to around 1,000 users. available for both web and mobile.
+                            </Card.Text>
+                                <a href="https://base.cm/">
+                                    <Button variant="primary">Chack the app</Button>
+                                </a>
+                            </Card.Body>
                         </div>
-                    </Card>
-                </CardDeck>
+                    
                 </div>
-                
+
                 <div className="Title-Ability" >
-                    <span style={{textAlign: 'center', fontSize: '3rem'}}>Works</span>
+                    <span style={{ textAlign: 'center', fontSize: '3rem' }}>Works</span>
                     <p className="Discription-Ability" >Works that I love and proud of.</p>
-                    <div style={{alignContent: 'center'}} className="Border-Ability" />
+                    <div style={{ alignContent: 'center' }} className="Border-Ability" />
                 </div>
 
                 <Carousel interval={null as any}>
                     <Carousel.Item>
                         <img
-                        className="Slids d-block w-100"
-                        src={MyArt}
-                        alt="First slide"
+                            className="Slids d-block w-100"
+                            src={MyArt}
+                            alt="First slide"
                         />
                         <Carousel.Caption>
-                        <h2 style={{fontStyle: 'Italic'}}>Seascape</h2>
-                        <p>Honolulu, Hwaii</p>
+                            <h2 style={{ fontStyle: 'Italic' }}>Seascape</h2>
+                            <p>Honolulu, Hwaii</p>
                         </Carousel.Caption>
                     </Carousel.Item>
 
                     <Carousel.Item>
                         <img
-                        className="Slids d-block w-100"
-                        src={MyArt2}
-                        alt="Third slide"
+                            className="Slids d-block w-100"
+                            src={MyArt2}
+                            alt="Third slide"
                         />
                         <Carousel.Caption>
-                        <h2 style={{fontStyle: 'Italic'}}>Memory in Summer</h2>
-                        <p>Kyoto, Japan</p>
+                            <h2 style={{ fontStyle: 'Italic' }}>Memory in Summer</h2>
+                            <p>Kyoto, Japan</p>
                         </Carousel.Caption>
                     </Carousel.Item>
 
                     <Carousel.Item>
                         <img
-                        className="Slids d-block w-100"
-                        src={MyArt3}
-                        alt="Third slide"
+                            className="Slids d-block w-100"
+                            src={MyArt3}
+                            alt="Third slide"
                         />
                         <Carousel.Caption >
-                        <div className="backgroundWork">
-                                <h2 style={{fontStyle: 'Italic'}}>Venus</h2>
+                            <div className="backgroundWork">
+                                <h2 style={{ fontStyle: 'Italic' }}>Venus</h2>
                                 <p>Vancouver, Canada</p>
-                        </div>
+                            </div>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             </div>
         );
     }
-} 
+}
 
 export default Works;
